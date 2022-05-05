9e60fe85c4a9c0029ba2ed8b0f39906fe681a25f
minhhau2423 <71492981+minhhau2423@users.noreply.github.com>
2022-05-05 08:49:20 +0700

    feature 1 test

--- uber-clone/App.js
+++ uber-clone/App.js
@@ -7,7 +7,7 @@ import RestaurantDetail from './screens/RestaurantDetail';
 
 export default function App() {
   return (
-    // navigation --ver 2
+    // navigation --feature 1
     <RootNavigation/>
   );
 }
